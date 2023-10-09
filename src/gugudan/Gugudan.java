c77d36402070aa0e90a233ba82e74c96af565508
Jaehyeokkang <kjhg2397@naver.com>
2023-10-09 21:20:26 +0900

    Gugudan-class

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -1,11 +1,24 @@
 package gugudan;
 
 public class Gugudan {
-	public static void main(String[] args) {
+	public static int[] calculate(int items) {
 		int [] gugu = new int[9];
 		for (int i = 0; i<9; i++) {
-		gugu[i] = 6 * (i+1);
-		System.out.println(gugu[i]);
+		gugu[i] = items * (i+1);
+		
+		}
+		return gugu;
+	}
+	public static void print(int[] result) {
+		for(int i=0; i<result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	public static void main(String[] args) {
+		for(int i=2; i<10; i++) {
+			int [] result = calculate(i);
+			print(result);
+		
 		}
 			
 		}
